868365a35786aef0b93a828f86076ed0772240a4
Utaka19 <185202718+Utaka19@>
2025-11-16 01:40:54 +0900

    vue3、vue router導入、ファイル編集

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,16 @@
 import './bootstrap';
+import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
+
+import App from './App.vue';
+
+const routes = [
+    { path: '/', component: () => import('./components/Hello.vue') }
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
+createApp(App).use(router).mount('#app');
---
+++ resources/js/components/Hello.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>Hello Vue + Laravel SPA</h1>
+</template>
